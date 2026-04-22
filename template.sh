dd1aeaa617de679fcdad1218918079fe2568f365
IT24102831 <it24102831@my.sliit.lk>
2026-04-22 21:25:19 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating directory
+mkdir -p src
+echo -p research
+
+#Creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch reqirements.txt
+
+echo "Directory and files created successfully!"
