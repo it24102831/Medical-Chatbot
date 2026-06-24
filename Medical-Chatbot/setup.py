bc0746d6190fbf44bfc18c58f3d7b0fdb9546491
IT24102831 <it24102831@my.sliit.lk>
2026-06-25 00:19:04 +0530

    requirements added

---
+++ Medical-Chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Charindu",
+    author_email="charinduindrasiri21@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
