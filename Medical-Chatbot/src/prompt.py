4167ec50986c7759de4cd7c110e484b92019a7a1
Charindu Sanjeeva Indrasiri Danwaththa <it24102831@my.sliit.lk>
2026-07-06 01:18:10 +0530

    Delete Medical-Chatbot directory

--- Medical-Chatbot/src/prompt.py
+++
@@ -1,9 +0,0 @@
-system_prompt = (
-    "You are a medical assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer the question. "
-    "If you don't know the answer, just say that you don't know. "
-    "Use a few sentences from the context to form your answer. If the context is not relevant to the question, say that you don't know. "
-    "Answer concisely."
-    "\n\n"
-    "{context}"
-)
