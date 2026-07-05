bfc1a81aa77d4290b78cf4d6e0667aae64d4df52
Charindu Sanjeeva Indrasiri Danwaththa <it24102831@my.sliit.lk>
2026-07-06 01:08:00 +0530

    Fix the errors.

--- Medical-Chatbot/template.sh
+++ Medical-Chatbot/template.sh
@@ -4,13 +4,13 @@ mkdir -p research
 
 
 #Creating files
-touch src/_init_.py
+touch src/__init__.py
 touch src/helper.py
 touch src/prompt.py
 touch .env
 touch setup.py
 touch app.py
 touch research/trials.ipynb
-touch reqirements.txt
+touch requirements.txt
 
 echo "Directory and files created successfully!"
